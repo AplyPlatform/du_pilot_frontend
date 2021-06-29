356d2f9ad1045820b2081205527a003a3f9cc57d
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-06-29 23:25:37 +0900

    전화번호 인증, 이메일 인증후 가입기능 추가

--- center/js/reg.js
+++ center/js/reg.js
@@ -11,38 +11,46 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-var langset = "KR";
+var g_str_cur_lang = "KR";
+// 인증여부 혜지프로
+var g_b_phonnumber_verified = false;
+var g_b_email_verified = false;
 
 $(function () {
     showLoader();
     $("#show_1").show();
 
-    langset = getCookie("language");
+    g_str_cur_lang = getCookie("language");
 
     $("#droneplay_name").val(getCookie("temp_name"));
     $("#droneplay_email").val(getCookie("temp_email"));
 
-    $("#droneplay_name").attr("placeholder", LANG_JSON_DATA[langset]['name_label']);
-    $("#droneplay_email").attr("placeholder", LANG_JSON_DATA[langset]['email_label']);
-    $("#droneplay_phonenumber").attr("placeholder", LANG_JSON_DATA[langset]['phone_label']);
+    $("#droneplay_name").attr("placeholder", GET_STRING_CONTENT('name_label'));
+    $("#droneplay_email").attr("placeholder", GET_STRING_CONTENT('email_label'));
+    $("#droneplay_phonenumber").attr("placeholder", GET_STRING_CONTENT('phone_label'));
 
-    $("#register_label").text(LANG_JSON_DATA[langset]['register_label']);
-    $("#privacy_link_label").text(LANG_JSON_DATA[langset]['privacy_link_label']);
-    $("#fill_info_label").text(LANG_JSON_DATA[langset]['fill_info_label']);
-    $("#register_explain_label").text(LANG_JSON_DATA[langset]['register_explain_label']);
-    $("#btnAgree").text(LANG_JSON_DATA[langset]['msg_agree']);
-    $("#btnRegisterToMember").text(LANG_JSON_DATA[langset]['msg_register']);
-    $("#btnBack1").text(LANG_JSON_DATA[langset]['msg_back']);
-    $("#btnBack2").text(LANG_JSON_DATA[langset]['msg_back']);
+    $("#register_label").text(GET_STRING_CONTENT('register_label'));
+    $("#privacy_link_label").text(GET_STRING_CONTENT('privacy_link_label'));
+    $("#tos_link_label").text(GET_STRING_CONTENT('tos_link_label'));
+    $("#fill_info_label").text(GET_STRING_CONTENT('fill_info_label'));
+    $("#register_explain_label").text(GET_STRING_CONTENT('register_explain_label'));
+    $("#btnAgree").text(GET_STRING_CONTENT('msg_agree'));
+    $("#btnRegisterToMember").text(GET_STRING_CONTENT('msg_register'));
+    $("#btnBack1").text(GET_STRING_CONTENT('msg_back'));
+    $("#btnBack2").text(GET_STRING_CONTENT('msg_back'));
 
 
     $("#show_2").hide();
     hideLoader();
 
+    // 전화번호 & 인증번호 입력제한 혜지프로
+    $('#droneplay_phonenumber').keypress(validateNumber);
+    $('#verification_code').keypress(validateNumber);
+
 });
 
 function onAgree() {
-    GATAGM('AgreeBtnClickOnRegister', 'CONTENT', langset);
+    GATAGM('AgreeBtnClickOnRegister', 'CONTENT');
     showLoader();
     $("#show_2").show();
     $("#show_1").hide();
@@ -50,20 +58,28 @@ function onAgree() {
 }
 
 function showPrivacyButton() {
-	  GATAGM('privacy_link_label', 'CONTENT', langset);
+	  GATAGM('privacy_link_label', 'CONTENT');
 
-	  $.get("/privacy_" + langset + "_raw.html", function(html_string){
+	  $.get("/privacy_" + g_str_cur_lang + "_raw.html", function(html_string){
+      showAlert(html_string);
+   	});
+}
+
+function showTOSButton() {
+	  GATAGM('tos_link_label', 'CONTENT');
+
+	  $.get("/service_" + g_str_cur_lang + "_raw.html", function(html_string){
       showAlert(html_string);
    	});
 }
 
 function goHomeButton(btnName) {
-    GATAGM(btnName, 'CONTENT', langset);
+    GATAGM(btnName, 'CONTENT');
     goHome();
 }
 
 function goHome() {
-    if (langset == "KR")
+    if (g_str_cur_lang == "KR")
         location.href = "/index.html?fromapp=" + getCookie("isFromApp");
     else
         location.href = "/index_en.html?fromapp=" + getCookie("isFromApp");
@@ -89,8 +105,165 @@ function ajaxRequest(data, callback, errorcallback) {
     });
 }
 
+// 인증기간 타이머 혜지프로
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    interval_timer = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+			clearInterval(interval_timer);
+            showAlert(GET_STRING_CONTENT('msg_phone_verification_timeout'));
+        }
+    }, 1000);
+}
+
+// 전화번호 인증 혜지프로
+function verifyPhoneNo(){
+    
+    // check if phone number starts with 01 and is total of 11 digits
+    let phone_number = $('#droneplay_phonenumber').val();
+    if((phone_number.length != 11) || phone_number.substring(0,2) !== '01'){
+        showAlert(GET_STRING_CONTENT('msg_wrong_phone_format'));
+        return;
+    }
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LfPn_UUAAAAAN-EHnm2kRY9dUT8aTvIcfrvxGy7', {action: 'action_name'}).then(function(token) {
+            // send phone verification
+            var jdata = {
+                "action": "member", 
+                "daction" : "validate_phonenumber", 
+                "phone_number" : phone_number, 
+                "g_token": token
+            };
+            ajaxRequest(jdata, 
+                function (data){
+                    let result = data.result_code;
+                    if(result === 0){ //정상응답
+                        showAlert(GET_STRING_CONTENT('msg_verification_code_sent'));
+                        g_b_phonnumber_verified = false;
+                        // 인증하기 텍스트 -> 재전송
+                        $('#btn_verify_code').text("재전송");
+                        var duration = 60 * 3;
+                        var display = $('#remaining_time');
+                        startTimer(duration, display);
+                        //$('#droneplay_phonenumber').prop( "disabled", true );
+                        $("#code_verification_input").show();
+                        return;
+                    }
+                    if (result === 2) {
+                        showAlert(GET_STRING_CONTENT('msg_wrong_phone_format'));
+                        return;
+                    }
+                    if (result === 3) {
+                        showAlert(GET_STRING_CONTENT('msg_phone_already_exists'));
+                        return;
+                    }
+                    showAlert(GET_STRING_CONTENT('msg_error_sorry'));
+                },
+                function (err, stat, error) {
+                    showAlert(GET_STRING_CONTENT('msg_error_sorry'));
+                }
+            );
+        });
+    });
+}
+
+function verifyCode(){
+    let verification_code = $('#verification_code').val();
+		if(verification_code == ""){
+			showAlert(GET_STRING_CONTENT('msg_code_empty'));
+			return;
+		} 
+		grecaptcha.ready(function() {
+			grecaptcha.execute('6LfPn_UUAAAAAN-EHnm2kRY9dUT8aTvIcfrvxGy7', {action: 'action_name'}).then(function(token) {
+				var jdata = {
+                    "action" : "member", 
+                    "daction" : "check_verifycode", 
+                    "phone_number" : $('#droneplay_phonenumber').val(), 
+                    "verify_code" : verification_code, 
+                    "g_token" : token};
+				ajaxRequest(jdata,
+					function(data){
+						let result = data.result_code;
+						if(result === 0){
+							$('#verification_code').val("");
+							$("#code_verification_input").hide();			
+							showAlert(GET_STRING_CONTENT('msg_phone_verified'));
+							clearInterval(interval_timer);
+							// disable phone number input
+                            g_b_phonnumber_verified = true;
+                            $('#auth_code').val(data.auth_code);
+							// $('#droneplay_phonenumber').prop( "disabled", true );
+							// $('btn_check_code').text("재인증");
+							return;
+						}
+						if(result === 2){
+							showAlert(GET_STRING_CONTENT('msg_wrong_verification_code'));
+							return;
+						}
+						if(result === 4){
+							showAlert(GET_STRING_CONTENT('msg_phone_verification_timeout'));
+							return;
+						}
+                        showAlert(GET_STRING_CONTENT('msg_error_sorry'));
+					},
+					function (err, stat, error) {
+						showAlert(GET_STRING_CONTENT('msg_error_sorry'));
+					}
+				);
+			});
+		});
+}
+
+// 이메일 인증 혜지프로
+function checkEmail(){
+    let email = $('#droneplay_email').val();
+    if(email == ""){
+        showAlert(GET_STRING_CONTENT('msg_email_empty'));
+        return;
+    } 
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LfPn_UUAAAAAN-EHnm2kRY9dUT8aTvIcfrvxGy7', {action: 'action_name'}).then(function(token) {
+            var jdata = {
+                "action" : "member", 
+                "daction" : "validate_email", 
+                "email" : $('#droneplay_email').val(),
+                "g_token" : token};
+            ajaxRequest(jdata,
+                function(data){
+                    let result = data.result_code;
+                    if(result === 0){		
+                        g_b_email_verified = true;
+                        showAlert(GET_STRING_CONTENT('msg_email_valid'));
+                        return;
+                    }
+                    if(result === 2){
+                        showAlert(GET_STRING_CONTENT('msg_email_invalid'));
+                        return;
+                    }
+                    if(result === 3){
+                        showAlert(GET_STRING_CONTENT('msg_email_already_exists'));
+                        return;
+                    }
+                    showAlert(GET_STRING_CONTENT('msg_error_sorry'));
+                },
+                function (err, stat, error) {
+                    showAlert(GET_STRING_CONTENT('msg_error_sorry'));
+                }
+            );
+        });
+    });
+}
+
 function requestRegister() {
-    GATAGM('RegisterBtnClickOnRegister', 'CONTENT', langset);
+    GATAGM('RegisterBtnClickOnRegister', 'CONTENT');
 
     showLoader();
 
@@ -104,33 +277,47 @@ function requestRegister() {
                 var droneplay_phonenumber = $('#droneplay_phonenumber').val();
 
                 if (droneplay_name == null || droneplay_name == "") {
-                    GATAGM('NameIsEmptyOnRegister', 'CONTENT', langset);
-                    showAlert(LANG_JSON_DATA[langset]['msg_input_name']);
+                    GATAGM('NameIsEmptyOnRegister', 'CONTENT');
+                    showAlert(GET_STRING_CONTENT('msg_input_name'));
                     hideLoader();
                     return;
                 }
 
                 if (droneplay_email == null || droneplay_email == "") {
-                    GATAGM('EmailIsEmptyOnRegister', 'CONTENT', langset);
-                    showAlert(LANG_JSON_DATA[langset]['msg_input_email']);
+                    GATAGM('EmailIsEmptyOnRegister', 'CONTENT');
+                    showAlert(GET_STRING_CONTENT('msg_input_email'));
                     hideLoader();
                     return;
                 }
 
                 if (droneplay_phonenumber == null || droneplay_phonenumber == "") {
-                    GATAGM('PhoneIsEmptyOnRegister', 'CONTENT', langset);
-                    showAlert(LANG_JSON_DATA[langset]['msg_input_phone']);
+                    GATAGM('PhoneIsEmptyOnRegister', 'CONTENT');
+                    showAlert(GET_STRING_CONTENT('msg_input_phone'));
                     hideLoader();
                     return;
                 }
 
+                // 전화번호 인증여부 체크 혜지프로
+                if(g_b_phonnumber_verified === false){
+                    GATAGM('PhoneNotVerified', 'CONTENT');
+                    showAlert(GET_STRING_CONTENT('msg_phone_not_verified'));
+                    hideLoader();
+                    return;
+                }
 
+                if(g_b_email_verified === false){
+                    GATAGM('EmailNotVerified', 'CONTENT');
+                    showAlert(GET_STRING_CONTENT('msg_email_not_verified'));
+                    hideLoader();
+                    return;
+                }
                 var data = {
                     "action": "member",
                     "daction": "register",
                     "name": droneplay_name,
                     "socialid": droneplay_email,
-                    "phone_number": droneplay_phonenumber,
+                    "phone_number" : droneplay_phonenumber,
+                    "auth_code": $('#auth_code').val(),
                     "sns_kind": sns_kind,
                     "sns_token": sns_token
                 };
@@ -146,17 +333,17 @@ function requestRegister() {
                     		$("#show_2").show();
                     		
                         if (r.result_code == 3 && r.reason.indexOf("socialid") >= 0) {
-	                            showAlert(LANG_JSON_DATA[langset]['msg_email_is_already_exist']);
-	                            GATAGM('EmailIsExistOnRegister', 'CONTENT', langset);  
+	                            showAlert(GET_STRING_CONTENT('msg_email_is_already_exist'));
+	                            GATAGM('EmailIsExistOnRegister', 'CONTENT');  
 	                            return;
                         }
 
-                        showAlert(LANG_JSON_DATA[langset]['msg_wrong_input']);
+                        showAlert(GET_STRING_CONTENT('msg_wrong_input'));
                     }
                 },
                     function (request, status, error) {
 
-                        GATAGM('ErroOnRegister_' + error, 'CONTENT', langset);
+                        GATAGM('ErroOnRegister_' + error, 'CONTENT');
                         showAlert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
                         //
                         hideLoader();
@@ -165,10 +352,21 @@ function requestRegister() {
     });
 }
 
+function validateNumber(event) {
+    var key = window.event ? event.keyCode : event.which;
+    if (event.keyCode === 8 || event.keyCode === 46) {
+        return true;
+    } else if ( key < 48 || key > 57 ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 function showConfirmDialog() {
-    $('#askModalLabel').text(LANG_JSON_DATA[langset]['modal_title']);
-    $('#askModalContent').text(LANG_JSON_DATA[langset]['msg_register_success']);
-    $('#askModalOKButton').text(LANG_JSON_DATA[langset]['modal_confirm_btn']);
+    $('#askModalLabel').text(GET_STRING_CONTENT('modal_title'));
+    $('#askModalContent').text(GET_STRING_CONTENT('msg_register_success'));
+    $('#askModalOKButton').text(GET_STRING_CONTENT('modal_confirm_btn'));
     $('#askModalCancelButton').hide();
 
     $('#askModalOKButton').off('click');
@@ -196,11 +394,17 @@ function getCookie(cName) {
 }
 
 function showAlert(msg) {
-    $('#modal-title').text(LANG_JSON_DATA[langset]['modal_title']);
-    $('#modal-confirm-btn').text(LANG_JSON_DATA[langset]['modal_confirm_btn']);
+    $('#modal-title').text(GET_STRING_CONTENT('modal_title'));
+    $('#modal-confirm-btn').text(GET_STRING_CONTENT('modal_confirm_btn'));
 
     $('#errorModalLabel').html(msg);
     $('#errorModal').modal('show');
+    if(msg == GET_STRING_CONTENT('msg_phone_verification_timeout')){
+        $('#modal-confirm-btn').off('click');
+        $('#modal-confirm-btn').click(function () {
+            location.href=location.href;
+        });
+    }
 }
 
 
@@ -212,16 +416,20 @@ function hideLoader() {
     $("#loading").fadeOut(800);
 }
 
-function GATAGM(label, category, language) {
+function GATAGM(label, category) {
     gtag(
-        'event', label + "_" + language, {
+        'event', label + "_" + g_str_cur_lang, {
         'event_category': category,
         'event_label': label
     }
     );
 
     mixpanel.track(
-        label + "_" + language,
+        label + "_" + g_str_cur_lang,
         { "event_category": category, "event_label": label }
     );
+}
+
+function GET_STRING_CONTENT(table_index_str) {	
+		return LANG_JSON_DATA[g_str_cur_lang][table_index_str];
 }
