d3ac8f4ffe4e9f643bd60d589dd63d448718814c
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2021-06-30 17:45:00 +0900

    첫화면의 비행기록 문구 수정

--- center/js/center.js
+++ center/js/center.js
@@ -1500,16 +1500,14 @@ function flightRecords2DMapInit() {
 				            radius: radius,
 				            fill: new ol.style.Fill({ color: '#FFF' }),
 				            stroke: new ol.style.Stroke({ color: '#45cdba', width: 2 })
-									})
-	              })];
-
-								if (size > 1) {
-									style[0].setText(new ol.style.Text({
+		                }),
+		              text: new ol.style.Text({
 					                  text: size.toString(),
 					                  fill: new ol.style.Fill({ color: '#000' }),
 					                  scale: 1.5
-									}));
-								}
+										})
+
+	              })];
 
 	          		styleCacheForCompany[size] = style
 				    }
@@ -1519,6 +1517,7 @@ function flightRecords2DMapInit() {
 
 
     g_vector_2D_map_for_flight_rec = new ol.source.Vector();
+
 		var clusterSource = new ol.source.Cluster({
 		  distance: 40,
 		  source: g_vector_2D_map_for_flight_rec,
@@ -1540,21 +1539,18 @@ function flightRecords2DMapInit() {
 			    size == 1 ? radius = 8 : radius = 10 + (size * 0.1);
 			    var style = styleCache[size];
 			    if (!style) {
-			        style = [new ol.style.Style({
-		                image: new ol.style.Circle({
-						            radius: radius,
-						            fill: new ol.style.Fill({ color: '#FFF' }),
-						            stroke: new ol.style.Stroke({ color: '#fb5b58', width: 2 })
-	                		})
-	              })];
-
-							if (size > 1) {
-								style[0].setText(new ol.style.Text({
+			       style = [new ol.style.Style({
+                image: new ol.style.Circle({
+			            radius: radius,
+			            fill: new ol.style.Fill({ color: '#FFF' }),
+			            stroke: new ol.style.Stroke({ color: '#fb5b58', width: 2 })
+	                }),
+	              text: new ol.style.Text({
 				                  text: size.toString(),
 				                  fill: new ol.style.Fill({ color: '#000' }),
 				                  scale: 1.5
-								}));
-							}
+								})
+              })];
 
 							styleCache[size] = style
 			    }
